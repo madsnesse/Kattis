8ca04964a83ab54030d836e38baf5ae339f65f3f
madsnesse <45721557+madsnesse@users.noreply.github.com>
2022-09-10 22:42:37 +0200

    added missing file

---
+++ .github/scripts/get_valid_files.py
@@ -0,0 +1,15 @@
+def main():
+    all_files = sys.args[0]
+    print(all_files)
+    valid_file_extensions = ['.c','.c++','.cc','.c#','.cpp','.cs','.cxx','.cbl','.cob','.cpy','.fs','.go','.h','.hs','.java','.js','.kt','.lisp','.cl','.m','.ml','.pas','.php','.pl','.rb','.rs','.scala','.f90','.f','.for']
+    result = []
+
+    for file in all_files:
+        if "."+file.split(".")[1] in valid_file_extensions:
+            result.append(file)
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
